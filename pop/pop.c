b3d50cd72c11271ce1b0b544c870d208220d93eb
SutharMahendra <omajimahendra@gmail.com>
2023-09-21 19:55:25 +0530

    pop fuction

---
+++ pop/pop.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max 4
+
+int stack_arr[max];
+int top=-1;
+int value;
+
+void push(int data){
+    if(top==max-1){
+        printf("stack is overflow");
+        return;
+    }else{
+        stack_arr[top]=data;
+        top = top +1;
+    }
+}
+
+void pop(int value){
+    if(top==-1){
+        printf("stack is under flow");
+        return;
+    }else{
+        value=stack_arr[top];
+        top =top -1;
+    }
+}
+
+void display(){
+    if(top==-1){
+        printf("stack is underflow");
+    }else{
+        for(int i=top; i>=0; i--){
+            printf("%d",stack_arr[i]);
+            printf("\n");
+        }
+    }
+}
+
+int main(){
+    int data;
+    push(3);
+    push(4);
+    push(6);
+    push(8);
+
+    pop(value);
+    display();
+
+    return 0;
+}
