cec485ce49c6fbfa9dc5a0a5610535321c2e105a
SutharMahendra <omajimahendra@gmail.com>
2023-12-21 09:38:06 +0530

    Push

---
+++ push.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#define max 4
+int stack_arr[max];
+    int top= -1; 
+
+void push(int data){
+   
+    if(top==max-1){
+        printf("Stack is overflow");
+        return;  
+    }
+   top=top+1;
+   stack_arr[top]=data;
+}
+
+int main(){
+    
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    
+
+    return 0;    
+
+}
