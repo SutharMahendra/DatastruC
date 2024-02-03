08dacb35029296ee42d8ebb89eb2424a8938d044
SutharMahendra <omajimahendra@gmail.com>
2024-02-03 10:58:16 +0530

    insertion opration at certain position

--- Linked_List/Certain_position_insert.c
+++ Linked_List/Certain_position_insert.c
@@ -1,39 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
     /* data */
     int data;
-    struct node* link;
+    struct node *link;
 };
 
-struct node* createnode(int data){
-    struct node* new;
-    new = (struct node*)malloc(sizeof(struct node));
+struct node *createnode(int data)
+{
+    struct node *new;
+    new = (struct node *)malloc(sizeof(struct node));
     new->data = data;
     new->link = NULL;
 
     return new;
 }
 
-struct node* certain_position_insert(struct node* head){
-    struct node* ptr;
+struct node *certain_position_insert(struct node *head)
+{
+    struct node *ptr;
     ptr = createnode(47);
     int n;
     printf("enter Nth position [0 to 3] :");
     scanf("%d", &n);
 
-    if(n==0){
-        ptr->link=head;
+    if (n == 0)
+    {
+        ptr->link = head;
         head = ptr;
-        
-    }else{
-        struct node* temp;
-        struct node* temp2;
-             temp = head;
-            temp2 = head;
-        int i=0;
+    }
+    else
+    {
+        struct node *temp;
+        struct node *temp2;
+        temp = head;
+        temp2 = head;
+        int i = 0;
         while (i != n && temp != NULL)
         {
             /* code */
@@ -42,43 +46,43 @@ struct node* certain_position_insert(struct node* head){
             i++;
         }
 
-        if(i == n){
+        if (i == n)
+        {
             temp2->link = ptr;
-            ptr->link = temp;  
-        }else {
+            ptr->link = temp;
+        }
+        else
+        {
             printf("invalid number\n");
         }
-        
-        
-    } 
-    return head; 
-
+    }
+    return head;
 }
 
-int main(){
-   struct node* head;
-   head = createnode(32);
+int main()
+{
+    struct node *head;
+    head = createnode(32);
 
-   struct node* ptr1;
-   ptr1 = createnode(45);
+    struct node *ptr1;
+    ptr1 = createnode(45);
 
-   head->link=ptr1;
+    head->link = ptr1;
 
-   struct node* ptr2;
-   ptr2 = createnode(49);
-   
-   ptr1->link=ptr2;
-   
-   
-   head=certain_position_insert(head);
-   
-   struct node* ptr;
-   ptr = head;
+    struct node *ptr2;
+    ptr2 = createnode(49);
 
-    while(ptr != NULL){
+    ptr1->link = ptr2;
+
+    head = certain_position_insert(head);
+
+    struct node *ptr;
+    ptr = head;
+
+    while (ptr != NULL)
+    {
         printf("%d\n", ptr->data);
         ptr = ptr->link;
     }
-    return 0; 
-   
+    return 0;
 }
